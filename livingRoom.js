5de8fcf4213108a54f4f3a64a85ae73f49a94a65
A-Sister <92495146+A-Sister@users.noreply.github.com>
2023-06-05 19:25:12 -0500

    Add files via upload

---
+++ livingRoom.js
@@ -0,0 +1,32 @@
+function preload() {
+    img = loadImage("living room.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(625, 500);
+    canvas.position(650, 300);
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects!";
+}
+
+function modelLoaded() {
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
+
+function draw(){
+    image(img, 0,0,625,500);
+}
+
+function back() {
+    window.location.href = "index.html"
+}
